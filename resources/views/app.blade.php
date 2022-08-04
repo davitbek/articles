7f4835db2ac87dd7f461504ec58f0babeedb6ae5
Davit <davitzeynalyan1991@gmail.com>
2022-08-04 20:29:27 +0400

    install botstrap

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+
+<div id="app"></div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
